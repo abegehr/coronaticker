9fc96eb70ec29c5a991c35e01e26177c8a8bb835
Anton Begehr <a.begehr@gmail.com>
2020-03-15 11:15:40 +0100

    fetch data and render in FutureBuilder

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() => runApp(MyApp());
 
@@ -38,20 +39,41 @@ class CountriesList extends StatefulWidget {
 }
 
 class CountriesListState extends State<CountriesList> {
-  final _data = [];
   final _biggerFont = const TextStyle(fontSize: 18.0);
 
-  Future<http.Response> fetchData() {
-    return http.get('https://corona.lmao.ninja/countries');
+  Future<List<Country>> fetchData() async {
+    final response = await http.get('https://corona.lmao.ninja/countries');
+
+    print(response);
+
+    if (response.statusCode == 200) {
+      final parsed = jsonDecode(response.body).cast<Map<String, dynamic>>();
+      return parsed.map<Country>((json) => Country.fromJson(json)).toList();
+    } else {
+      throw Exception('Failed to fetch data');
+    }
   }
 
   @override
   Widget build(BuildContext context) {
+    return FutureBuilder<List<Country>>(
+      future: fetchData(),
+      builder: (context, snapshot) {
+        if (snapshot.hasError) print(snapshot.error);
+
+        return snapshot.hasData
+            ? _buildList(snapshot.data)
+            : Center(child: CircularProgressIndicator());
+      },
+    );
+  }
+
+  Widget _buildList(data) {
     return ListView.builder(
         padding: const EdgeInsets.all(16.0),
         itemBuilder: (context, i) {
-          if (i < _data.length) {
-            return _buildRow(_data[i]);
+          if (i < data) {
+            return _buildRow(data[i]);
           }
           return null;
         });
