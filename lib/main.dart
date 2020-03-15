779385097b0841d45d97e762b11ccc80b3a0ebfb
Anton Begehr <a.begehr@gmail.com>
2020-03-15 10:59:18 +0100

    write Country class

--- lib/main.dart
+++ lib/main.dart
@@ -66,3 +66,35 @@ class CountriesListState extends State<CountriesList> {
     );
   }
 }
+
+class Country {
+  // {"country":"China","cases":80849,"todayCases":25,"deaths":3199,"todayDeaths":10,"recovered":66915,"critical":3226}
+  final String name;
+  final int cases;
+  final int todayCases;
+  final int deaths;
+  final int todayDeaths;
+  final int recovered;
+  final int critical;
+
+  Country(
+      {this.name,
+      this.cases,
+      this.todayCases,
+      this.deaths,
+      this.todayDeaths,
+      this.recovered,
+      this.critical});
+
+  factory Country.fromJson(Map<String, dynamic> json) {
+    return Country(
+      name: json['name'],
+      cases: json['cases'],
+      todayCases: json['todayCases'],
+      deaths: json['deaths'],
+      todayDeaths: json['todayDeaths'],
+      recovered: json['recovered'],
+      critical: json['critical'],
+    );
+  }
+}
