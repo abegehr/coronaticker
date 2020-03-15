ed4b42945c109070bb1adbe511a377a72aa40ad3
Adrian Pfeiffer-Ruiz <adrian.pfeiffer.ruiz@web.de>
2020-03-15 23:27:53 +0100

    not showing

--- lib/components/CoutryRow.dart
+++ lib/components/CoutryRow.dart
@@ -2,19 +2,99 @@ import 'package:flutter/material.dart';
 import 'package:coronaticker/model/Country.dart';
 
 class CountryRow extends StatelessWidget {
-  final _biggerFont = const TextStyle(fontSize: 18.0);
   final Country country;
 
-  CountryRow({Key key, this.country}) : super(key: key);
+  const CountryRow({
+    Key key,
+    this.country,
+    this.name,
+    this.cases,
+    this.todayCases,
+  });
+
+  final String name;
+  final int cases;
+  final int todayCases;
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+  final _smallerFont = const TextStyle(fontSize: 14.0);
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-        leading: Image.asset("assets/images/flags/china.png"),
-        title: Text(
-          country.toString(),
-          style: _biggerFont,
-        ),
-        onTap: () => print("Tapped ${country.name}"));
+    return Padding(
+        padding: const EdgeInsets.fromLTRB(5.0, 0.0, 0.0, 0.0),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Expanded(
+              flex: 2,
+              child: Image.asset("assets/images/flags/china.png"),
+            ),
+            Expanded(
+                flex: 9,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    Text(
+                      country.name,
+                      style: _biggerFont,
+                    ),
+                    Expanded(
+                        flex: 1,
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: <Widget>[
+                            Expanded(
+                              flex: 1,
+                              child: Column(
+                                children: <Widget>[
+                                  Text(
+                                    country.todayCases.toString(),
+                                    style: _smallerFont,
+                                  ),
+                                  Text(
+                                    country.cases.toString(),
+                                    style: _biggerFont,
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Expanded(
+                              flex: 1,
+                              child: Column(
+                                children: <Widget>[
+                                  Text(
+                                    country.todayCases.toString(),
+                                    style: _smallerFont,
+                                  ),
+                                  Text(
+                                    country.cases.toString(),
+                                    style: _biggerFont,
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Expanded(
+                              flex: 1,
+                              child: Column(
+                                children: <Widget>[
+                                  Text(
+                                    country.todayCases.toString(),
+                                    style: _smallerFont,
+                                  ),
+                                  Text(
+                                    country.cases.toString(),
+                                    style: _biggerFont,
+                                  ),
+                                ],
+                              ),
+                            )
+                          ],
+                        ))
+                  ],
+                )),
+          ],
+        ));
   }
 }
