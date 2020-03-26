0be793cf37fcb2654bb6b343e25aac86d0b9dea7
Anton Begehr <a.begehr@gmail.com>
2020-03-26 16:32:45 +0100

    Pass all uncaught errors from the framework to Crashlytics.
    https://github.com/FirebaseExtended/flutterfire/tree/master/packages/firebase_crashlytics#use-the-plugin

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:coronaticker/pages/CountriesPage.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  // Pass all uncaught errors from the framework to Crashlytics.
+  FlutterError.onError = Crashlytics.instance.recordFlutterError;
+
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
