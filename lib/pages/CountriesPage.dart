01799034d500a3fab784e703b77d9ef78ec445f5
Anton Begehr <a.begehr@gmail.com>
2020-05-02 15:15:07 +0200

    add link to Impressum

--- lib/pages/CountriesPage.dart
+++ lib/pages/CountriesPage.dart
@@ -1,5 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:coronaticker/components/CountriesList.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+launchImpressum() async {
+  const url = 'https://places.rocks/impressum';
+  if (await canLaunch(url)) {
+    await launch(url);
+  } else {
+    throw 'Could not launch $url';
+  }
+}
 
 class CountriesPage extends StatelessWidget {
   CountriesPage({Key key, this.title}) : super(key: key);
@@ -12,7 +22,25 @@ class CountriesPage extends StatelessWidget {
       appBar: AppBar(
         title: Text(title),
       ),
-      body: Center(child: CountriesList()),
+      body: Column(
+        children: <Widget>[
+          Expanded(child: CountriesList()),
+          Align(
+            alignment: Alignment.center,
+            child: RaisedButton(
+              color: Colors.white10,
+              onPressed: launchImpressum,
+              child: Text(
+                'Impressum',
+                style: TextStyle(
+                  fontSize: 14,
+                  color: Colors.grey,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
